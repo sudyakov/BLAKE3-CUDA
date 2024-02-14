967cb91ea3e0a6d4b5367b2dd8250b60bb36e3aa
Sudyakov <sudyakov_git@icloud.com>
2024-02-14 13:51:39 +0200

    ~

--- cuda/src/main.rs
+++ cuda/src/main.rs
@@ -1,10 +1,12 @@
 use std::time::Instant;
 use std::vec::*;
 
-use async_cuda::memory::{DeviceBuffer, HostBuffer};
 use async_cuda::ffi::device::Device;
 use async_cuda::ffi::npp::context::Context;
+use async_cuda::memory::{DeviceBuffer, HostBuffer};
 use async_cuda::stream::Stream;
+use async_cuda::*;
+
 //use async_cuda::DeviceBuffer;
 
 // функиция принимает теcтовый вектор и возвращаетпо отсортированый по убыванию на ЦПУ
@@ -15,30 +17,29 @@ fn sort_cpu(vec: Vec<i32>) -> Vec<i32> {
 }
 
 // функиция принимает теcтовый вектор и возвращаетпо отсортированый по убыванию на ГПУ
-async fn sort_gpu(vec: Vec<i32>, stream: &Stream) -> Vec<i32> {
+async fn sort_gpu(vec: &Vec<i32>, stream: &Stream) -> Result<Vec<i32>, async_cuda::Error> {
     // создаем контекст и стрим
     let context = Context::from_null_stream();
-    let stream = Stream::new().await.unwrap();
+    //let stream = Stream::new().await.unwrap();
     // Создаем буферы на ЦПУ и ГПУ
-    let host_buffer = HostBuffer::from_slice(&vec).await;
-    let device_buffer = DeviceBuffer::from_slice(&vec[..], &stream).await;
-    //TODO: Call sorting kernel
-    let mut sorted_vec = vec;
+    let host_buffer = HostBuffer::from_slice(vec).await;
+    let mut device_buffer = DeviceBuffer::from_slice(&vec[..], &stream).await.unwrap();
+
     // Создаем контекст для вызова функци
-    
+    let context = Context::from_null_stream();
+    // Запускаем сортировку на ГПУ
+    let stream = Stream::new().await.unwrap();
+    unsafe {}
+    // Копируем отсортированный массив с ГПУ на ЦПУ
+    stream.synchronize().await;
+    let host_sorted_result = host_buffer.copy_to(&mut device_buffer, &stream).await;
 
-    sorted_vec
+    let host_vector: Result<Vec<i32>, async_cuda::Error> = match host_sorted_result {
+        Ok(vec) => Ok(Vec::new()),
+        Err(e) => Err(e),
+    };
+    host_vector
 }
-
-
-
-
-
-
-
-
-
-
 
 // Основная функция
 #[tokio::main]
@@ -61,11 +62,19 @@ pub async fn main() {
         cpu_result[cpu_result.len() - 1]
     );
 
+    // создаем стрим
+    let stream = Stream::new().await.unwrap();
+
     // функиция принимает теcтовый вектор и возвращаетпо отсортированый по убыванию на ГПУ
-    //let gpu_result = sort_gpu(test_vec.clone());
-    // Выводим первые и последние элементы массива gpu_sorted_vec
-    println!("First GPU sorted element: {}", gpu_sorted_vec[0]);
-    println!("Last GPU sorted element: {}", gpu_sorted_vec[gpu_sorted_vec.len() - 1]);
+    let gpu_result = sort_gpu(&test_vec, &stream).await;
+
+    // Выводим первые и последние элементы массива
+    // println!("First GPU sorted element: {}", gpu_result[0]);
+    // println!(
+    // "Last GPU sorted element: {}",
+    // gpu_result[gpu_result.len() - 1]
+    // );
+
 
     println!("Done!");
 }
