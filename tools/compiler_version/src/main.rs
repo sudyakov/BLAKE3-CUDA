7afa56d243e0bac4b86aabfe0bda7d6aa22f43f3
Sudyakov <sudyakov_git@icloud.com>
2024-02-22 15:50:22 +0200

    ~

--- tools/compiler_version/src/main.rs
+++ tools/compiler_version/src/main.rs
@@ -1,6 +1,12 @@
 use std::process::Command;
 
 fn main() {
+
+    let build = cc::Build::new();
+    let compiler = build.get_compiler();
+    let compiler_path = compiler.path().to_string_lossy();
+    println!("cargo:rustc-env=COMPILER_PATH={}", compiler_path);
+
     // Print the rustc version.
     Command::new(env!("CARGO"))
         .args(&["rustc", "--quiet", "--", "--version"])
